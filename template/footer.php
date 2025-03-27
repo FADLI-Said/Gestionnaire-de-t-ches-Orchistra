6ebd109385bd7010d6a57b59fe1beb2a42f2fdb9
Isaac Sehim <isaacsehim@gmail.com>
2025-03-27 11:52:26 +0100

    footer ok

--- template/footer.php
+++ template/footer.php
@@ -1,4 +1,9 @@
 <footer>
-    <p>Orchistra © 2025 - Tous droits réservés</p>
+    <div>
+        <a href="https://www.facebook.com/"><i class="fab fa-facebook"></i></a>
+        <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+        <a href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a>        
+        <p>Orchistra © 2025 - Tous droits réservés</p>
+    </div>
 </footer>
 <script src="https://kit.fontawesome.com/50a1934b21.js" crossorigin="anonymous"></script>
